2e11aa56e564b362651c6512ad6ec3471a99afee
rafaela-eduarda <149601165+rafaela-eduarda@users.noreply.github.com>
2024-04-11 16:45:45 -0300

    88

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const dataAtual = new Date();
 let dataNiver = prompt
 ("Data do próximo niver:");
 dataNiver = new Data (dataNiver+"T23:59:59");
+let diasQueFaltam = Math.floor ((dataNiver-dataAtual)/86400000);
+document.querySelector("#dias_restantes").textcontent=diasQuefaltam;
